6481b1900fe962cb5eb5e9c7484554e4c299f1d3
Eunchul <eunchul.dev@gmail.com>
2020-07-08 01:03:56 +0900

    pass the test

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # sqsqs
-Simple Quick SQS wrapper
+SQSQS(Simple Quick SQS wrapper)
+
+# Usage
+```
+const Queue = require("sqsqs")
+
+const data1 = "data1"
+const data2 = "data2"
+
+/* Send any number of string */
+/* Warning: the order of messages are not kept in the batch list! */
+await Queue.send([data1, data2])
+
+/* Receive specific number of string */
+/* it might wait until fetch queueSize messages or there are no remain messages in SQS queue */
+let queueSize = 50
+let received = await Queue.receive(queueSize)
+
+/* ***
+received = [{
+  Body: "data1",
+  ReceiptHandleId: ...,
+  Id: ...
+},{
+  Body: "data2",
+  ReceiptHandleId: ...,
+  Id: ...
+}
+]
+*** */
+
+/* Delete Messages */
+await Queue.delete(received);
+
+```
---
+++ index.js
@@ -0,0 +1,73 @@
+const aws = require("aws-sdk");
+const { v4: uuidv4 } = require('uuid');
+
+function chunk(arr, chunk_size) {
+    var R = [];
+    for (var i=0,len=arr.length; i<len; i+=chunk_size)
+        R.push(arr.slice(i,i+chunk_size));
+    return R;
+}
+
+
+class Queue {
+  constructor(option) {
+    this.queueOption = {
+      QueueUrl: option.QueueUrl,
+      MaxNumberOfMessages: option.MaxNumberOfMessages || 10,
+      VisibilityTimeout: option.VisibilityTimeout,
+      WaitTimeSeconds: option.WaitTimeSeconds || 20,
+    };
+    this.awsOption = {
+      apiVersion: option.apiVersion,
+      region: option.region,
+    }
+    this.sqs = new aws.SQS(this.awsOption);
+  }
+  async send(messages) {
+    let res = await Promise.all(chunk(messages, 10).map(messagesChunk => 
+      this.sqs.sendMessageBatch({
+        QueueUrl: this.queueOption.QueueUrl,
+        Entries: messagesChunk.map(m => ({
+          Id: uuidv4(),
+          MessageBody: m
+        })),
+      }).promise()))
+    return res;
+  }
+  async receive(queueSize=10) {
+    let messages = [];
+    while(true) {
+      let res = await this.sqs.receiveMessage({
+        QueueUrl: this.queueOption.QueueUrl,
+        MaxNumberOfMessages: Math.min(this.queueOption.MaxNumberOfMessages, queueSize - messages.length),
+        VisibilityTimeout: this.queueOption.VisibilityTimeout,
+        WaitTimeSeconds: Math.min(this.queueOption.WaitTimeSeconds),
+      }).promise();
+      if(!res.Messages || res.Messages.length == 0)
+        break;
+      for(let msg of res.Messages){
+        try{
+          messages.push(msg);
+        } catch(e) {
+          console.log(`Fail to decode message '${msg.Body}', just skip it`);
+        }
+      }
+      if(messages.length >= queueSize)
+        break;
+    }
+    return messages;
+  }
+  async delete(messages) {
+    let res = await Promise.all(chunk(messages, 10).map(messagesChunk =>
+      this.sqs.deleteMessageBatch({
+        QueueUrl: this.queueOption.QueueUrl,
+        Entries: messagesChunk.map(m => ({
+          Id: m.MessageId,
+          ReceiptHandle: m.ReceiptHandle ,
+        })),
+      }).promise()));
+    return res;
+  }
+}
+
+module.exports = Queue;
